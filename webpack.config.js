304a6a3837aad7a2d0ec4ad070e64106f190ec81
Byeonggyu Shin <94808683+byeonggyu-shin@users.noreply.github.com>
2022-09-27 15:02:04 +0900

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/
+      }
+    ]
+  }
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     template: "./publlic/index.html",
+  //   }),
+  // ],
+}
